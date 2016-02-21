44ff9e17871d4abe5004a64d35778339859873e8
Brandon Mabey <brandonmabey@gmail.com>
2016-02-21 01:31:04 +0000

    asd

--- app/main.py
+++ app/main.py
@@ -165,7 +165,7 @@ def getMap(data):
         grid[wall[1]][wall[0]] = Map.wall
 
     for coin in data.get('gold', []):
-        grid[coin[1]][coin[0]] = Map.coin
+        grid[coin[1]][coin[0]] = Coin()
 
     for food in data.get('food', []):
         grid[food[1]][food[0]] = Food()
