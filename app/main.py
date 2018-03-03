3388542582896b3c481f6090ad5db7baee4f87b7
brayden-arthur <braydenarthur@gmail.com>
2018-03-03 12:15:16 -0800

    print food

--- app/main.py
+++ app/main.py
@@ -358,6 +358,7 @@ def emergencyFoodCalc(data, head, snake, map):
     move = None
     if (snake['health'] < 75):
         print("Need food now!")
+        print(data['food']['data'])
     	food = map(lambda n: [n['y'], n['x']], data['food']['data'])
         pathing_point = [0,0]
         if (len(food) > 0):
