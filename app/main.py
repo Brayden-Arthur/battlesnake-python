39204c15c1a27edb197e13021719ad5e22330cef
brayden-arthur <braydenarthur@gmail.com>
2018-03-03 12:05:12 -0800

    testing mapping

--- app/main.py
+++ app/main.py
@@ -358,7 +358,7 @@ def emergencyFoodCalc(data, head, snake, map):
     move = None
     if (snake['health'] < 75):
         print("Need food now!")
-	food = map(data['food']['data'], lambda x: [x['y'], x['x']])
+    	food = map(lambda n: [n['y'], n['x']], data['food']['data'])
         pathing_point = [0,0]
         if (len(food) > 0):
             pathing_point = min(food, key = lambda foodPoint: dist(head, foodPoint))
